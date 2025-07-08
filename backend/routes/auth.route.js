7c890610ffe8c26f3e4a2871f1fc03685d31ad03
Rithvik <sairithvik.achutuni@students.iiit.ac.in>
2025-07-08 20:43:09 +0530

    trying to be able to change profile pic

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import {protectRoute} from '../middleware/auth.middleware.js'
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 
-//BgVVqyiFmPBgZOqF
+router.put('/update', protectRoute, updateProfile);
 
 export default router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 const app = express();
 
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 import authRoutes from '../routes/auth.route.js';
 import {connectDB} from "../lib/db.js";
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
