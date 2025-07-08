b6b3f52205433237562f39eb75aa1e0aa028b8d9
Rithvik <sairithvik.achutuni@students.iiit.ac.in>
2025-07-09 03:43:45 +0530

    able to send messages

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,4 +1,10 @@
+import {useRef, useState} from "react";
+
 const MessageInput = () => {
+    const [text, setText] = useState("");
+    const [imagePreview, setImagePreview] = useState(null);
+    const fileInputRef = useRef(null);
+
 
 }
 
