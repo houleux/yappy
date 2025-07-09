b8c090a54e0a72d2eccfe8b59cdd3be4bf484b4b
Rithvik <sairithvik.achutuni@students.iiit.ac.in>
2025-07-09 06:03:50 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,11 @@ import { app, server } from "./lib/socket.js";
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import * as path from "node:path";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -20,6 +23,13 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
